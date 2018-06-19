1be98565946ebc0d1709d61a53aba09de0812e5b
stalingit <stalinmca2008@gmail.com>
2018-06-19 18:18:54 +0530

    Add files via upload
    updated by stalin on 19 june18

---
+++ New Toys/app.js
@@ -0,0 +1,23 @@
+App = function()
+{
+      // wade.setLoadingImages('loading.jpg');
+    
+    // this is where the WADE app is initialized
+	this.init = function()
+	{
+	    
+        // load a scene
+        wade.loadScene('preload.wsc', false, function()
+        {
+		wade.loadScene('scene1.wsc', false, function()
+        {
+            // the scene has been loaded, do something here
+
+        });
+        });
+	};
+	this.load =function(){
+	
+	wade.loadAudio('SmallExplosion8-Bit.ogg');
+	};
+};
